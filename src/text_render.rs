eb7cb424f753447882a8c50f5447e36e7b72bdec
Stian Soltvedt <stian@soltvedt.info>
2019-02-28 12:08:17 +0100

    First working version

--- src/text_render.rs
+++ src/text_render.rs
@@ -1,9 +1,9 @@
-use image::{ImageBuffer, Rgba};
+use image::{ImageBuffer, Pixel, Rgba};
 use rusttype::{point, Font, Scale};
 use std::ops::DerefMut;
 
 const FONT_DATA: &[u8] = include_bytes!("../fonts/OpenSans-Regular.ttf");
-const FONT_SCALE: f32 = 32.0;
+const FONT_SCALE: f32 = 18.0;
 
 pub struct TextRenderer<'a> {
     font: Font<'a>,
@@ -20,9 +20,15 @@ impl<'a> TextRenderer<'a> {
         }
     }
 
-    pub fn render_text<T>(&self, text: &str, image: &mut ImageBuffer<image::Rgba<u8>, T>)
-    where
-        T: DerefMut<Target = [<image::Rgba<u8> as image::Pixel>::Subpixel]>,
+    pub fn render_text<T>(
+        &self,
+        text: &str,
+        offset_x: u32,
+        offset_y: u32,
+        right_align: bool,
+        image: &mut ImageBuffer<Rgba<u8>, T>,
+    ) where
+        T: DerefMut<Target = [<Rgba<u8> as image::Pixel>::Subpixel]>,
     {
         let v_metrics = self.font.v_metrics(self.scale);
 
@@ -33,8 +39,8 @@ impl<'a> TextRenderer<'a> {
             .collect();
 
         // work out the layout size
-        let _glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
-        let _glyphs_width = {
+        let glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
+        let glyphs_width = {
             let min_x = glyphs
                 .first()
                 .map(|g| g.pixel_bounding_box().unwrap().min.x)
@@ -46,25 +52,28 @@ impl<'a> TextRenderer<'a> {
             (max_x - min_x) as u32
         };
 
+        let offset_x = if right_align {
+            offset_x - glyphs_width
+        } else {
+            offset_x
+        };
+        let offset_y = offset_y - (glyphs_height as f32 * 1.6) as u32;
+
         // Loop through the glyphs in the text, positing each one on a line
         for glyph in glyphs {
             if let Some(bounding_box) = glyph.pixel_bounding_box() {
                 // Draw the glyph into the image per-pixel by using the draw closure
                 glyph.draw(|x, y, v| {
-                    image.put_pixel(
-                        // Offset the position by the glyph bounding box
-                        x + bounding_box.min.x as u32,
-                        y + bounding_box.min.y as u32,
-                        // Turn the coverage into an alpha value
-                        Rgba {
-                            data: [
-                                self.colour.0,
-                                self.colour.1,
-                                self.colour.2,
-                                (v * 255.0) as u8,
-                            ],
-                        },
-                    )
+                    let x = offset_x + x + bounding_box.min.x as u32;
+                    let y = offset_y + y + bounding_box.min.y as u32;
+
+                    let new_pixel = Rgba([
+                        self.colour.0,
+                        self.colour.1,
+                        self.colour.2,
+                        (v * 255.0) as u8,
+                    ]);
+                    image.get_pixel_mut(x, y).blend(&new_pixel);
                 });
             }
         }
